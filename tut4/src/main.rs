4d4bca94f9dabc8514118480283b22a9eec6125f
AyushNautiyalDeveloper <ayush@betalectic.com>
2023-12-27 23:13:34 +0530

    leaned data types

--- tut4/src/main.rs
+++ tut4/src/main.rs
@@ -2,9 +2,12 @@ fn main() {
     // let a : f32 = -2.44;
     // println!("Hello, world! {}" , a);
 
-    let mut tup: (i32, &str, bool) = (3, "h", true);
-    tup.0 = 4;
-    println!("{}", tup.0);
+    // let mut tup: (i32, &str, bool) = (3, "h", true);
+    // tup.0 = 4;
+    // println!("{}", tup.0);
+
+    //array dont get initialized by themself.
+    // let mut arr: [i32; 4]; this wont work.
 
     let mut arr: [i32; 4] = [1, 2, 3, 4];
     print!(" {} ", arr[0]);
